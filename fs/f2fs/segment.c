8a555b54f841c25797ca664265937ef196b0ae51
bengris32 <bengris32@protonmail.ch>
2022-11-27 18:58:24 +0000

    fs/: f2fs: Import f2fs changes from selene-r-oss
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I633d5452a0c413dc1053195bb7127d2d2b416289

--- fs/f2fs/segment.c
+++ fs/f2fs/segment.c
@@ -3,6 +3,7 @@
  * fs/f2fs/segment.c
  *
  * Copyright (c) 2012 Samsung Electronics Co., Ltd.
+ * Copyright (C) 2021 XiaoMi, Inc.
  *             http://www.samsung.com/
  */
 #include <linux/fs.h>
@@ -603,8 +604,6 @@ repeat:
 	if (kthread_should_stop())
 		return 0;
 
-	sb_start_intwrite(sbi->sb);
-
 	if (!llist_empty(&fcc->issue_list)) {
 		struct flush_cmd *cmd, *next;
 		int ret;
@@ -624,8 +623,6 @@ repeat:
 		}
 		fcc->dispatch_list = NULL;
 	}
-
-	sb_end_intwrite(sbi->sb);
 
 	wait_event_interruptible(*q,
 		kthread_should_stop() || !llist_empty(&fcc->issue_list));
